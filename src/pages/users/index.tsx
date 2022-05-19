769d5c9bf11087f91a94497bdb5af864fe01cdc1
damarisdwayne <damarisdwayne@outlook.com>
2022-05-19 19:59:16 -0300

    adicionando react query ao projeto

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,12 +1,52 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react';
-import React from 'react';
-import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import React, { useEffect } from 'react';
+import { useQuery } from 'react-query';
+import { RiAddLine } from 'react-icons/ri';
+import Link from 'next/link';
+
 import { Pagination } from '../../components/Pagination';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/SideBar';
-import Link from 'next/link';
+
+import {
+    Box,
+    Button,
+    Checkbox,
+    Flex,
+    Heading,
+    Icon,
+    Spinner,
+    Table,
+    Tbody,
+    Td,
+    Text,
+    Th,
+    Thead,
+    Tr,
+    useBreakpointValue
+
+} from '@chakra-ui/react';
 
 export default function UserList() {
+    const { data, isLoading, error } = useQuery('users', async () => {
+        const response = await fetch('http://localhost:3000/api/users')
+        const data = await response.json()
+
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                })
+            }
+        })
+
+        return users
+    })
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true
@@ -35,34 +75,49 @@ export default function UserList() {
                             </Button>
                         </Link>
                     </Flex>
+                    {isLoading ? (
+                        <Flex justify='center'>
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify='center'>
+                            <Text>Falha ao obter dados do usuário</Text>
+                        </Flex>
+                    ) : (
+                        <>
+                            <Table>
+                                <Thead>
+                                    <Tr>
+                                        <Th px={['4', '4', '6']} color='gray.300' width={8}>
+                                            <Checkbox colorScheme='pink' />
+                                        </Th>
+                                        <Th>Usuários</Th>
+                                        {isWideVersion && <Th>Data de cadastro</Th>}
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {data.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={['4', '4', '6']}>
+                                                    <Checkbox colorScheme='pink' />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight='bold'>{user.name}</Text>
+                                                        <Text fontSize='sm' color='gray.300'>{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td>{user.createdAt}</Td>}
+                                            </Tr>
+                                        )
+                                    })}
+                                </Tbody>
+                            </Table>
 
-                    <Table>
-                        <Thead>
-                            <Tr>
-                                <Th px={['4', '4', '6']} color='gray.300' width={8}>
-                                    <Checkbox colorScheme='pink' />
-                                </Th>
-                                <Th>Usuários</Th>
-                                {isWideVersion && <Th>Data de cadastro</Th>}
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px={['4', '4', '6']}>
-                                    <Checkbox colorScheme='pink' />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight='bold'>Dâmaris Dwayne</Text>
-                                        <Text fontSize='sm' color='gray.300'>damarisdwayne@outlook.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>04 de Abril, 2022</Td>}
-                            </Tr>
-                        </Tbody>
-                    </Table>
-
-                    <Pagination />
+                            <Pagination />
+                        </>
+                    )}
                 </Box>
             </Flex>
         </Box>
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,5 @@
-import { createServer, Model } from 'miragejs'
+import { createServer, Factory, Model } from 'miragejs'
+import { faker } from '@faker-js/faker';
 
 type User = {
     name: string
@@ -12,6 +13,26 @@ export function makeServer() {
             user: Model.extend<Partial<User>>({})
         },
 
+        factories: { //gera dados em massa
+            user: Factory.extend({
+                name(i: number){
+                    return `User ${i + 1}`
+                },
+
+                email(){
+                    return faker.internet.email().toLowerCase()
+                },
+
+                createdAt(){
+                    return faker.date.recent(10)
+                },
+            })
+        },
+
+        seeds(server) { //dados fake para aplicação
+            server.createList('user', 10)
+        },
+
         routes() {
             this.namespace = 'api'
             this.timing = 750
@@ -19,7 +40,8 @@ export function makeServer() {
             this.get('/users')
             this.post('/users')
 
-            this.namespace = ''
+            this.namespace = '' //reseta o endpoint para que possamos criar a apasta api em pages
+            this.passthrough()
         }
     })
 
